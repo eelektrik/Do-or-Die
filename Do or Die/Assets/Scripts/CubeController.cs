c284aec5bd2503191e0b79cbf2e49d36d2d1dc2e
David Jenkins <eelektrik1138@hotmail.com>
2020-03-28 14:42:05 -0700

    Pick ups updated

---
+++ Do or Die/Assets/Scripts/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float speed = 5f;
+    public float height = 0.5f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        tempPos = transform.position;
+        float newY = Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(tempPos.x, newY, tempPos.z) * height;
+    }
+}
